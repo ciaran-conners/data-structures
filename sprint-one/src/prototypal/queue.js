a45dc52758f80464b046a1035f69829220a5d010
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-05-17 23:01:49 -0400

    Up to Pseudoclassical Classes

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.nextIndex = 0;
+  instance.frontIndex = 0;
+  instance.storage = {};
+  return instance;
 };
 
-var queueMethods = {};
+var enqueue = function(value) {
+  this.storage[this.nextIndex] = value;
+  this.nextIndex++;
+};
+
+var dequeue = function() {
+  if (this.size()) {
+    var result = this.storage[this.frontIndex];
+    delete this.storage[this.frontIndex];
+    this.frontIndex++;
+    return result;
+  }
+};
+
+var size = function() {
+  return Object.keys(this.storage).length;
+};
+
+
+
+var queueMethods = {
+  enqueue: enqueue,
+  dequeue: dequeue,
+  size: size
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.nextIndex = 0;
+  instance.storage = {};
+  return instance;
 };
 
-var stackMethods = {};
+var push = function(value) {
+  this.storage[this.nextIndex] = value;
+  this.nextIndex++;
+};
+
+var pop = function() {
+  if (this.size() > 0) {
+    this.nextIndex--;
+    var result = this.storage[this.nextIndex];
+    delete this.storage[this.nextIndex];
+    return result;
+  }
+};
+
+var size = function() {
+  return Object.keys(this.storage).length;
+};
+
+var stackMethods = {
+  push: push,
+  pop: pop,
+  size: size
+};
 
 
