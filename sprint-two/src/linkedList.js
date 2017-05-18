86db07a340e1ac3a81a09510e0554b6810859399
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-05-18 19:14:24 -0400

    done with linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head) {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail = newNode;
+      list.head = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      var toRemove = list.head.value;
+      list.head = list.head.next;
+      return toRemove;
+    }
   };
 
-  list.contains = function(target) {
-  };
+  list.contains = function(target, currentNode) {
+    var currentNode = currentNode || list.head;
+    if (currentNode.value === target) {
+      return true;
+    } else {
+      currentNode = currentNode.next;
+      if (currentNode) {
+        return list.contains(target, currentNode);
+      }
+    }
+    return false;
+   };
 
   return list;
 };
